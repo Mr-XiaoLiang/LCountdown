2531024b5115c72d3dcc4df68562a1120c28a027
lollipop <liujian594@hotmail.com>
2020-05-19 00:20:18 +0800

    add: 添加渐变色生成工具

---
+++ mobile/src/main/java/liang/lollipop/lcountdown/activity/GradientActivity.kt
@@ -0,0 +1,16 @@
+package liang.lollipop.lcountdown.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import liang.lollipop.lcountdown.R
+
+/**
+ * 渐变色生成的Activity
+ */
+class GradientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gradient)
+    }
+}

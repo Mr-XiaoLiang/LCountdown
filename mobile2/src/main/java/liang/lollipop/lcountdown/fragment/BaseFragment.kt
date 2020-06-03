111c5ceb4090bac0a2f9ac4fed7b197af08f557a
lollipop <liujian594@hotmail.com>
2020-06-04 00:34:45 +0800

    Add: 添加基础的Fragment

--- mobile2/src/main/java/liang/lollipop/lcountdown/fragment/BaseFragment.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/fragment/BaseFragment.kt
@@ -1,10 +1,42 @@
 package liang.lollipop.lcountdown.fragment
 
 import androidx.fragment.app.Fragment
+import liang.lollipop.lcountdown.listener.*
 
 /**
  * @author lollipop
  * @date 2020/6/3 00:28
  */
-class BaseFragment: Fragment() {
+class BaseFragment: Fragment(),
+        BackPressedListener,
+        BackPressedProvider,
+        OnWindowInsetsProvider {
+
+    private val windowInsetsProviderHelper: WindowInsetsProviderHelper by lazy {
+        WindowInsetsProviderHelper()
+    }
+
+    private val backPressedProviderHelper: BackPressedProviderHelper by lazy {
+        BackPressedProviderHelper()
+    }
+
+    override fun onBackPressed(): Boolean {
+        return backPressedProviderHelper.onBackPressed()
+    }
+
+    override fun addBackPressedListener(listener: BackPressedListener) {
+        backPressedProviderHelper.addBackPressedListener(listener)
+    }
+
+    override fun removeBackPressedListener(listener: BackPressedListener) {
+        backPressedProviderHelper.removeBackPressedListener(listener)
+    }
+
+    override fun addOnWindowInsetsProvider(listener: OnWindowInsetsListener) {
+        windowInsetsProviderHelper.addOnWindowInsetsProvider(listener)
+    }
+
+    override fun removeOnWindowInsetsProvider(listener: OnWindowInsetsListener) {
+        windowInsetsProviderHelper.removeOnWindowInsetsProvider(listener)
+    }
 }

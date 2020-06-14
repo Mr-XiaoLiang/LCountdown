8c280dbae5e97f7418f6cd9b2c7d888fe774160a
lollipop <liujian594@hotmail.com>
2020-06-14 22:40:43 +0800

    开始小部件调整页面的框架开发

---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/fragment/adjustment/CardAdjustmentFragment.kt
@@ -0,0 +1,29 @@
+package liang.lollipop.lcountdown.fragment.adjustment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import liang.lollipop.lcountdown.R
+import liang.lollipop.lcountdown.fragment.BaseFragment
+
+/**
+ * @author lollipop
+ * @date 2020/6/14 22:01
+ */
+abstract class CardAdjustmentFragment: BaseFragment() {
+
+    abstract val layoutId: Int
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_adjustment_base, container, false)
+        if (layoutId != 0) {
+            inflater.inflate(layoutId, rootView.findViewById(R.id.panelBodyGroup))
+        }
+        return rootView
+    }
+
+}
---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/fragment/adjustment/TimeAdjustmentFragment.kt
@@ -0,0 +1,17 @@
+package liang.lollipop.lcountdown.fragment.adjustment
+
+import liang.lollipop.lcountdown.R
+
+/**
+ * @author lollipop
+ * @date 2020/6/14 22:09
+ * 时间调整的碎片
+ */
+class TimeAdjustmentFragment: CardAdjustmentFragment() {
+    override val layoutId = R.layout.fragment_adjustment_time
+
+    override val title = R.string.title_time
+
+    override val colorId = R.color.focusTimeAdjust
+
+}

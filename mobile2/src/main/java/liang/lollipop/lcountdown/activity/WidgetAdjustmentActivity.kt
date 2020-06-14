1c68ace0b6a44e201bfeab16b99b4958c42beccf
lollipop <liujian594@hotmail.com>
2020-06-14 21:52:57 +0800

    开始小部件调整页面的框架开发

--- mobile2/src/main/java/liang/lollipop/lcountdown/activity/WidgetAdjustmentActivity.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/activity/WidgetAdjustmentActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_widget_adjustment.*
 import liang.lollipop.lcountdown.R
-import liang.lollipop.lcountdown.listener.WindowInsetsHelper
 import liang.lollipop.lcountdown.util.BottomSheetHelper
-import liang.lollipop.lcountdown.util.log
 
 /**
  * 小部件的调整页面
@@ -20,7 +18,7 @@ class WidgetAdjustmentActivity : BaseActivity() {
         setContentView(R.layout.activity_widget_adjustment)
         initRootGroup(rootGroup)
         bottomSheetHelper = BottomSheetHelper(sheetPanel, panelGrip, widgetFrame, sheetContent)
-        bottomSheetHelper?.hide(false)
+        bottomSheetHelper?.show(false)
         panelGrip.setOnClickListener {
             bottomSheetHelper?.changeStatus()
         }
@@ -29,7 +27,6 @@ class WidgetAdjustmentActivity : BaseActivity() {
     override fun onInsetsChange(root: View, left: Int, top: Int, right: Int, bottom: Int) {
         tabGroup.setPadding(left, 0, right, bottom)
         bottomSheetHelper?.paddingBottom = bottom
-        log("bottom:", bottom)
     }
 
     override fun onStop() {

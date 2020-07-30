b9ce4a892d68cc03a5cb8b7ae47dd31387673990
lollipop <liujian594@hotmail.com>
2020-07-31 00:03:23 +0800

    推进位置调整面板进度

---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/fragment/adjustment/LocationAdjustmentFragment.kt
@@ -0,0 +1,16 @@
+package liang.lollipop.lcountdown.fragment.adjustment
+
+/**
+ * @author lollipop
+ * @date 7/30/20 23:42
+ */
+class LocationAdjustmentFragment: CardAdjustmentFragment() {
+    override val layoutId: Int
+        get() = TODO("Not yet implemented")
+    override val icon: Int
+        get() = TODO("Not yet implemented")
+    override val title: Int
+        get() = TODO("Not yet implemented")
+    override val colorId: Int
+        get() = TODO("Not yet implemented")
+}

2a445e7bcf3ab78192a2adc9f9a74d88ece83ff2
lollipop <liujian594@hotmail.com>
2020-08-08 21:46:09 +0800

    推进调整面板进度

--- mobile2/src/main/java/liang/lollipop/lcountdown/view/ShapeView.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/view/ShapeView.kt
@@ -1,8 +1,11 @@
 package liang.lollipop.lcountdown.view
 
 import android.content.Context
+import android.graphics.Outline
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewOutlineProvider
+import liang.lollipop.lcountdown.drawable.GradientDrawable
 
 /**
  * @author lollipop
@@ -16,6 +19,40 @@ class ShapeView(context: Context, attr: AttributeSet?,
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context) : this(context, null)
 
+    private val gradientDrawable = GradientDrawable()
 
+    private var corner = 0F
+
+    private val myOutlineProvider = object : ViewOutlineProvider() {
+        override fun getOutline(view: View?, outline: Outline?) {
+            view?:return
+            outline?:return
+            outline.setRoundRect(view.left, view.top, view.right, view.bottom, corner)
+        }
+    }
+
+    init {
+        background = gradientDrawable
+    }
+
+    fun changeColor(vararg colors: Int) {
+        gradientDrawable.changeColor(*colors)
+    }
+
+    fun changeColor(colors: List<Int>) {
+        gradientDrawable.changeColor(colors)
+    }
+
+    fun changeStart(x: Float, y: Float) {
+        gradientDrawable.changeStart(x, y)
+    }
+
+    fun changeEnd(x: Float, y: Float) {
+        gradientDrawable.changeEnd(x, y)
+    }
+
+    override fun getOutlineProvider(): ViewOutlineProvider {
+        return myOutlineProvider
+    }
 
 }

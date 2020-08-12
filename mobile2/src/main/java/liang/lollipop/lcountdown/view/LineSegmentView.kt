747583c6497d89b7c03829ebea1a9c0e67a04a6e
lollipop <liujian594@hotmail.com>
2020-08-13 01:14:48 +0800

    推进调整面板进度

--- mobile2/src/main/java/liang/lollipop/lcountdown/view/LineSegmentView.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/view/LineSegmentView.kt
@@ -1,9 +1,21 @@
 package liang.lollipop.lcountdown.view
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
 /**
  * @author lollipop
  * @date 8/12/20 01:04
  * 线段的View
  */
-class LineSegmentView {
+class LineSegmentView(context: Context, attr: AttributeSet?,
+                      defStyleAttr: Int, defStyleRes: Int) : View(context, attr, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+
+
 }

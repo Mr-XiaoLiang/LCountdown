41c17842419b113a30b62c23cf3c616c87d170ef
lollipop <liujian594@hotmail.com>
2020-09-25 00:05:44 +0800

    增加杂项更新

---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/activity/AboutActivity.kt
@@ -0,0 +1,9 @@
+package liang.lollipop.lcountdown.activity
+
+import liang.lollipop.lcountdown.R
+
+class AboutActivity : AppBarActivity() {
+    override val layoutId: Int
+        get() = R.layout.activity_about
+
+}

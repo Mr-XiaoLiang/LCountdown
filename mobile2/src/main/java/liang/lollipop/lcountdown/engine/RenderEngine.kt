a378aee059b59643d33c58ca59c7c571149a209e
lollipop <liujian594@hotmail.com>
2020-09-30 00:53:53 +0800

    推进时间信息面板

---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/engine/RenderEngine.kt
@@ -0,0 +1,14 @@
+package liang.lollipop.lcountdown.engine
+
+import android.graphics.Canvas
+
+/**
+ * @author lollipop
+ * @date 9/30/20 00:36
+ * 渲染引擎
+ */
+abstract class RenderEngine {
+
+    abstract fun draw(canvas: Canvas)
+
+}

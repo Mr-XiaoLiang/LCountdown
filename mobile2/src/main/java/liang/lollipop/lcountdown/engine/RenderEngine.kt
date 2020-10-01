18d2e2bbad4f8802019786eeff57ae87590c17be
lollipop <liujian594@hotmail.com>
2020-10-02 01:32:59 +0800

    推进时间信息面板

--- mobile2/src/main/java/liang/lollipop/lcountdown/engine/RenderEngine.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/engine/RenderEngine.kt
@@ -1,6 +1,7 @@
 package liang.lollipop.lcountdown.engine
 
 import android.graphics.Canvas
+import android.view.View
 
 /**
  * @author lollipop
@@ -13,5 +14,14 @@ abstract class RenderEngine {
      * 绘制的方法
      */
     abstract fun draw(canvas: Canvas)
+
+    protected fun layout(view: View, width: Int, height: Int) {
+        view.layout(0, 0, width, height)
+    }
+
+    protected fun measure(view: View, width: Int, height: Int) {
+        view.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY))
+    }
 
 }

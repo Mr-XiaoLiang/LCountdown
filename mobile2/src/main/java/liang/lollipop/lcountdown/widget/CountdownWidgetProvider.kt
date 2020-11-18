576873398155a9f9bac920bf9b3285ce9a13780b
lollipop <liujian594@hotmail.com>
2020-11-18 21:44:01 +0800

    推进小部件工具开发进度

---
+++ mobile2/src/main/java/liang/lollipop/lcountdown/widget/CountdownWidgetProvider.kt
@@ -0,0 +1,40 @@
+package liang.lollipop.lcountdown.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+/**
+ * @author lollipop
+ * @date 11/18/20 21:28
+ */
+class CountdownWidgetProvider: AppWidgetProvider() {
+
+    companion object {
+
+        const val WIDGET_SHOW = "WIDGET_SHOW"
+
+    }
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+
+    override fun onRestored(context: Context?, oldWidgetIds: IntArray?, newWidgetIds: IntArray?) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds)
+    }
+
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
+    }
+
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+    }
+
+    override fun onDisabled(context: Context?) {
+        super.onDisabled(context)
+    }
+
+
+}

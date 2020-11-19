29e65d0c47e9da98060d65900bf388016e0e4189
lollipop <liujian594@hotmail.com>
2020-11-19 21:08:48 +0800

    推进小部件更新进度

--- mobile2/src/main/java/liang/lollipop/lcountdown/widget/CountdownWidget.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/widget/CountdownWidget.kt
@@ -3,6 +3,11 @@ package liang.lollipop.lcountdown.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.graphics.Bitmap
+import android.widget.FrameLayout
+import liang.lollipop.lcountdown.engine.WidgetEngine
+import liang.lollipop.lcountdown.info.WidgetInfo
+import liang.lollipop.lcountdown.util.WidgetDBUtil
 
 /**
  * @author lollipop
@@ -11,13 +16,29 @@ import android.content.Context
 open class CountdownWidget: AppWidgetProvider() {
 
     companion object {
-
         const val WIDGET_SHOW = "WIDGET_SHOW"
-
     }
 
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
+        context?:return
+        appWidgetManager?:return
+        appWidgetIds?:return
+        val dbUtil = WidgetDBUtil.read(context)
+        val widgetInfo = WidgetInfo()
+        val widgetEngine = WidgetEngine(FrameLayout(context))
+        val bitmapProvider = BitmapProvider()
+        for (id in appWidgetIds) {
+            widgetInfo.widgetId = id
+            dbUtil.findByWidgetId(widgetInfo)
+            widgetEngine.updateAll(widgetInfo)
+            updateWidget(widgetEngine, bitmapProvider)
+        }
+        dbUtil.close()
+    }
+
+    private fun updateWidget(widgetEngine: WidgetEngine, bitmapProvider: BitmapProvider) {
+
     }
 
     override fun onRestored(context: Context?, oldWidgetIds: IntArray?, newWidgetIds: IntArray?) {
@@ -36,5 +57,34 @@ open class CountdownWidget: AppWidgetProvider() {
         super.onDisabled(context)
     }
 
+    private class BitmapProvider {
+        private val bitmapList = ArrayList<Bitmap>()
+
+        fun get(width: Int, height: Int): Bitmap {
+            for (bitmap in bitmapList) {
+                if (!bitmap.isRecycled
+                        && bitmap.width == width
+                        && bitmap.height == height) {
+                    return bitmap
+                }
+            }
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            bitmapList.add(bitmap)
+            return bitmap
+        }
+
+        fun recycle() {
+            for (bitmap in bitmapList) {
+                try {
+                    if (!bitmap.isRecycled) {
+                        bitmap.recycle()
+                    }
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
+            bitmapList.clear()
+        }
+    }
 
 }

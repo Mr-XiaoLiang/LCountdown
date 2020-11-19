55670ec966c0334897ce1a1cfcb8ded09949b7e7
lollipop <liujian594@hotmail.com>
2020-11-19 21:21:13 +0800

    推进小部件更新进度

--- mobile2/src/main/java/liang/lollipop/lcountdown/widget/CountdownWidget.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/widget/CountdownWidget.kt
@@ -1,10 +1,16 @@
 package liang.lollipop.lcountdown.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.widget.FrameLayout
+import android.widget.RemoteViews
+import liang.lollipop.lcountdown.R
+import liang.lollipop.lcountdown.activity.WidgetAdjustmentActivity
 import liang.lollipop.lcountdown.engine.WidgetEngine
 import liang.lollipop.lcountdown.info.WidgetInfo
 import liang.lollipop.lcountdown.util.WidgetDBUtil
@@ -28,17 +34,34 @@ open class CountdownWidget: AppWidgetProvider() {
         val widgetInfo = WidgetInfo()
         val widgetEngine = WidgetEngine(FrameLayout(context))
         val bitmapProvider = BitmapProvider()
+        val layoutId = R.layout.widget_desktop
         for (id in appWidgetIds) {
-            widgetInfo.widgetId = id
-            dbUtil.findByWidgetId(widgetInfo)
-            widgetEngine.updateAll(widgetInfo)
-            updateWidget(widgetEngine, bitmapProvider)
+            try {
+                widgetInfo.widgetId = id
+                dbUtil.findByWidgetId(widgetInfo)
+                widgetEngine.updateAll(widgetInfo)
+
+                val bitmap = bitmapProvider.new(widgetInfo.width.toInt(), widgetInfo.height.toInt())
+                val canvas = Canvas(bitmap)
+                widgetEngine.draw(canvas)
+
+                val remoteViews = RemoteViews(context.packageName, layoutId)
+                remoteViews.setImageViewBitmap(R.id.widgetPanel, bitmap)
+                //创建点击意图
+                val intent = WidgetAdjustmentActivity.createIntent(context, id)
+                //请求ID重复会导致延时意图被覆盖，刷新模式表示覆盖的方式
+                val pendingIntent = PendingIntent.getActivity(context, id,
+                        intent, PendingIntent.FLAG_UPDATE_CURRENT)
+                //为小部件设置点击事件
+                remoteViews.setOnClickPendingIntent(R.id.widgetPanel, pendingIntent)
+
+                appWidgetManager.updateAppWidget(id, remoteViews)
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
         }
+        bitmapProvider.recycle()
         dbUtil.close()
-    }
-
-    private fun updateWidget(widgetEngine: WidgetEngine, bitmapProvider: BitmapProvider) {
-
     }
 
     override fun onRestored(context: Context?, oldWidgetIds: IntArray?, newWidgetIds: IntArray?) {
@@ -68,6 +91,10 @@ open class CountdownWidget: AppWidgetProvider() {
                     return bitmap
                 }
             }
+            return new(width, height)
+        }
+
+        fun new(width: Int, height: Int): Bitmap {
             val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
             bitmapList.add(bitmap)
             return bitmap

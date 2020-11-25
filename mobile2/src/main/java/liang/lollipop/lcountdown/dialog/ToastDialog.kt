e55c6b78698a3152a7e5bdff34306357b25d4279
lollipop <liujian594@hotmail.com>
2020-11-25 21:48:10 +0800

    推进小部件更新进度

--- mobile2/src/main/java/liang/lollipop/lcountdown/dialog/ToastDialog.kt
+++ mobile2/src/main/java/liang/lollipop/lcountdown/dialog/ToastDialog.kt
@@ -1,25 +1,55 @@
 package liang.lollipop.lcountdown.dialog
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import liang.lollipop.lcountdown.R
 import liang.lollipop.lcountdown.listener.OnWindowInsetsListener
+import liang.lollipop.lcountdown.listener.WindowInsetsHelper
+import liang.lollipop.lcountdown.util.task
 import java.lang.RuntimeException
 import java.util.*
+import kotlin.math.abs
 
 /**
  * @author lollipop
  * @date 11/23/20 21:50
  */
-class ToastDialog: OnWindowInsetsListener {
+class ToastDialog: OnWindowInsetsListener,
+        ValueAnimator.AnimatorUpdateListener,
+        Animator.AnimatorListener {
 
     companion object {
         private const val LAYOUT_ID = R.layout.dialog_toast
         private const val TEXT_ID = R.id.toastText
         private const val ACTION_ID = R.id.actionBtn
+        private const val CONTENT_ID = R.id.toastCard
+
+        private const val TIMEOUT_OFF = -1L
+
+        private const val CONTENT_NONE = 0
+
+        private const val ANIMATION_DURATION = 300L
+
+        /**
+         * 进度精确到1%，小于1%认为已经是0了
+         */
+        private const val PROGRESS_HIDE = 0.01F
+
+        /**
+         * 进度精确到1%，大于99%认为已经是1了
+         */
+        private const val PROGRESS_SHOWN = 0.99F
+
+        /**
+         * 进度的阈值
+         */
+        private const val PROGRESS_THRESHOLD = 0.5F
 
         private fun findGroup(rootGroup: View?): ViewGroup? {
             rootGroup?:return null
@@ -54,58 +84,150 @@ class ToastDialog: OnWindowInsetsListener {
 
     private var toastView: View? = null
 
+    private var insetsHelper: WindowInsetsHelper? = null
+
     private var thisToast: ToastTask? = null
 
     private var pendingToast: ToastTask? = null
 
     private var progress = 0F
 
-    fun show(activity: Activity,
-             text: Int, action: Int, callback: (DismissEvent) -> Unit) {
-        val rootView = toastView?:createView(activity)
-        val shownToast = thisToast
-        if (shownToast != null) {
-            pendingToast = ToastTask(text, action, callback)
-            dismiss()
-            return
-        }
-        // TODO
+    private var touchable = false
+
+    private val animator = ValueAnimator().apply {
+        duration = ANIMATION_DURATION
+        addUpdateListener(this@ToastDialog)
+        addListener(this@ToastDialog)
     }
 
-    fun dismiss() {
-        // TODO
+    private val timeoutTask = task {
+        dismiss(DismissEvent.TimeOut)
+    }
+
+    private fun show(activity: Activity,
+             text: Int, delay: Long, action: Int, callback: (DismissEvent) -> Unit) {
+        toastView?:createView(activity)
+        showToast(ToastTask(text, delay, action, callback))
+    }
+
+    private fun showToast(task: ToastTask) {
+        val rootView = toastView?:return
+        val shownToast = thisToast
+        if (shownToast != null) {
+            pendingToast = task
+            dismiss(DismissEvent.Replace)
+            return
+        }
+        thisToast = task
+        rootView.findViewById<TextView>(TEXT_ID).setText(task.text)
+        val actionBtn = rootView.findViewById<TextView>(ACTION_ID)
+        if (task.action == CONTENT_NONE) {
+            actionBtn.visibility = View.GONE
+        } else {
+            actionBtn.visibility = View.VISIBLE
+            actionBtn.setText(task.action)
+        }
+        doAnimation(true, task.delay)
+    }
+
+    private fun dismiss(event: DismissEvent) {
+        thisToast?.callback?.invoke(event)
+        thisToast = null
+        doAnimation(false)
+    }
+
+    private fun doAnimation(isOpen: Boolean, timeoutOffset: Long = 0) {
+        // 动画开始，锁定手指触摸，防止手势打断操作
+        touchable = false
+        animator.cancel()
+        val endValue = if (isOpen) { 1F } else { 0F }
+        val duration = (abs(endValue - progress) * ANIMATION_DURATION).toLong()
+        timeoutTask.cancel()
+        // 如果是打开操作并且激活超时，那么设置超时
+        // 可能存在手势操作中断，使用展开动画归位时，使得toast延时，属于正常现象
+        if (isOpen && timeoutOffset >= 0) {
+            timeoutTask.delay(duration + timeoutOffset)
+        }
+        animator.setFloatValues(progress, endValue)
+        animator.duration = duration
+        animator.start()
     }
 
     private fun createView(activity: Activity): View {
         val rootGroup = findGroup(activity.window.decorView)?:throw RuntimeException(
                 "Root group not found")
-        val rootView = activity.layoutInflater.inflate(
+        val self = activity.layoutInflater.inflate(
                 LAYOUT_ID, null, false)
-        toastView = rootView
-        rootView.visibility = View.INVISIBLE
-        rootGroup.addView(rootView,
+        toastView = self
+        self.visibility = View.INVISIBLE
+        rootGroup.addView(self,
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT)
-        return rootView
+        insetsHelper = WindowInsetsHelper(self)
+        return self
+    }
+
+    private fun onAnimationEnd(isOpen: Boolean) {
+        // TODO
     }
 
     fun destroy() {
-
+        animator.cancel()
+        timeoutTask.cancel()
+        // TODO
     }
 
     override fun onInsetsChange(root: View, left: Int, top: Int, right: Int, bottom: Int) {
-        TODO("Not yet implemented")
+        insetsHelper?.updateByPadding(root, left, top, right, bottom)
     }
 
     private data class ToastTask(
             val text: Int,
+            val delay: Long,
             val action: Int,
-            val callback: (DismissEvent) -> Unit) {
-
-    }
+            val callback: (DismissEvent) -> Unit)
 
     enum class DismissEvent {
         TimeOut, Action, Swipe, Replace
+    }
+
+    override fun onAnimationUpdate(animation: ValueAnimator?) {
+        if (animation == animator) {
+            progress = animation.animatedValue as Float
+            val contentView = toastView?.findViewById<View>(CONTENT_ID)?:return
+            val height = contentView.top + contentView.height
+            contentView.translationY = height * (1 - progress)
+        }
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+        if (animation == animator) {
+            toastView?.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        if (animation == animator) {
+            when {
+                progress < PROGRESS_HIDE -> {
+                    onAnimationEnd(false)
+                }
+                progress > PROGRESS_SHOWN -> {
+                    onAnimationEnd(true)
+                }
+                else -> {
+                    doAnimation(progress > PROGRESS_THRESHOLD)
+                }
+            }
+        }
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+        // 取消了动画，那么不做任何处理
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+        // 动画重复播放，不做处理
     }
 
 }
